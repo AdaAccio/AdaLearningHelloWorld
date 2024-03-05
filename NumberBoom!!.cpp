0ac6d68009bd9ec18e11fce6a59ecfa1752ace8f
Ada <144916232+AdaAccio@users.noreply.github.com>
2024-03-05 19:07:44 +0800

    数字炸弹
    昨天第一天学C++,看到书上有生成随机数的教程就随手写了一个。有许多不完善的地方，尽力还原了高中时期大家趁着课间围成一圈玩这个游戏的规则，数字爆炸之后可以随机解锁某个倒霉孩子的情史（笑），一段苦中作乐的时光，再也回不去了。

---
+++ NumberBoom!!.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+	srand(time(0));
+	int num = rand() % 100;
+	
+	int min = 0;
+	int max = 100;
+	while(1)
+	{
+		int x;
+		cout << "£¨"<<min<<"~"<<max<<")\n";
+		cin >> x;
+		if (x > max || x < min)
+			cout << "(#`O¡ä)²»½²ÎäµÂ"<<endl;
+		else if (x < num)
+		{
+			//cout << x << '~' << max<<endl;
+			min = x;
+		}
+		else if (x > num)
+		{
+			//cout << min << "~" << x<<endl;
+			max = x;
+		}
+		else if (x == num)
+		{
+			cout << "Boom!!"<<endl;
+			break;
+		}
+		
+	}
+}
